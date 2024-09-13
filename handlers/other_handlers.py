f7f51c2fec0ed13b44e4d877f390b873bbc4a66a
Viktoriya Diukina <viki_dyukina@mail.ru>
2024-09-13 09:37:07 +0400

    commiting

---
+++ handlers/other_handlers.py
@@ -0,0 +1,18 @@
+from aiogram import Router
+
+from aiogram.types import Message
+import lexicon
+
+
+# Инициализируем роутер уровня модуля
+router = Router()
+
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@router.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=lexicon.LEXICON_RU['no_echo'])
